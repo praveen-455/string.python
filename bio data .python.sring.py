79b4087e239b57f25d4973ce28f18f5cae4e1706
praveen-455 <113237197+praveen-455@users.noreply.github.com>
2022-09-13 17:46:52 +0530

    Add files via upload

---
+++ bio data .python.sring.py
@@ -0,0 +1,44 @@
+
+first_name      = input("enter thr first name : ")
+last_name       = input("enter the last name : ")
+date_of_birth   = input("enter the date of birth : ")
+mobile_number   = input("enter the mobile number : ")
+email_id        = input("enter the email id :")
+family          = input("enter the family :")
+btech           = input("enter the btech :")
+inter           = input("enter the inter :")
+ssc             = input("enter the ssc :")
+addres          = input("enter the addres :")
+hobbies         = input("enter the hobbies :")
+
+print("____BIO DATA____")
+first name="FIRST NAME" .ljust(20)
+last name="LASTNAME" .ljust(20)
+date of birth="DATE OF BIRTH" .ljust(20)
+mobile number="MOBILE NUMBER" .ljust(20)
+email id="EMAIL ID" .ljust(20)
+family="FAMILY" .ljust(20)      
+btech="BTECH".ljust(20) 
+inter="INTER" .ljust(20)
+ssc"SSC" .ljust(20)
+addres"ADDRES" .ljust(20)
+hobbies"HOBBIES" .ljust(20)
+
+print(first name,":",first_name)
+print(last name,":",last_name)
+print(date of birth,":",date_of_birth)
+print(mobile number,":",mobile_number)
+print(email id,":",email_id)
+print(family,":",family)
+print(btech,":",btech)
+print(inter,":",inter)
+print(ssc,":",ssc)
+print(addres,":",addres)
+print(hobbies,":",hobbies)
+
+
+
+
+
+
+
