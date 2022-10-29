3c87d9eeaeeb80a7aaf869f57897a9ab9bd2a397
praveen-455 <113237197+praveen-455@users.noreply.github.com>
2022-10-29 14:58:13 +0530

    Add files via upload

---
+++ bio data .python.py
@@ -0,0 +1,38 @@
+first_name      = input("enter the first name : ") 
+last_name       = input("enter the last name : ") 
+date_of_birth   = input("enter the date of birth : ")
+mobile_number   = input("enter the mobile number : ")
+email_id        = input("enter the email id :")
+family          = input("enter the family :")
+btech           = input("enter the btech :")
+inter           = input("enter the inter :")
+ssc             = input("enter the ssc :")
+addres          = input("enter the addres :")
+hobbies        = input("enter the hobbies :")
+technical_skills= input("enter the technical :")
+
+
+("____BIO DATA____")
+print("FIRST NAME" .ljust(20),":",first_name.strip())
+print("LASTNAME" .ljust(20),":",last_name.strip())
+print("DATE OF BIRTH" .ljust(20),":",date_of_birth.strip())
+print("MOBILE NUMBER" .ljust(20),":",mobile_number.strip())
+print("EMAIL ID" .ljust(20),":",email_id.strip())
+print("FAMILY" .ljust(20),":",family.strip())      
+print("BTECH".ljust(20),":",btech.strip()) 
+print("INTER" .ljust(20),":",inter.strip())
+print("SSC" .ljust(20),":",ssc.strip())
+print("ADDRES" .ljust(20),":",addres.strip())
+print("HOBBIES" .ljust(20),":",hobbies.strip())
+print("TECHNICAL SKILLS" .ljust(20),":",technical_skills.strip())
+
+
+
+
+
+
+
+
+
+
+
